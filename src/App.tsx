e9d6c502cd5d8d7bdbeedb07b42a2a1a8a45185c
samuel <st0814511@gmail.com>
2025-11-03 20:01:45 +0100

    nil

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import SignUp from "./pages/auth/SignUp";
 import Login from "./pages/auth/Login";
 import AboutUs from "./components/AboutUs";
 import ScrollToTop from "./components/ScrollTopTop";
+import Sample from "./components/sample"
 
 const App = () => {
   return (
@@ -24,6 +25,7 @@ const App = () => {
         </Route>
         <Route path="/auth/sign-up" element={<SignUp />} />
         <Route path="/auth/login" element={<Login />} />
+        <Route path="/sample" element={<Sample />} />
         {/* other pages routes that won't have the header and footer will be defined here */}
       </Routes>
     </>
